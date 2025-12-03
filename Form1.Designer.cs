776192125be046fbbca609f1914337471f26ba1f
Unfinished632 <username1653e632@gmail.com>
2025-12-03 16:19:15 +0300

    Added user generated qr codes and adjusted things

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,29 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.qrImage = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.qrImage)).BeginInit();
+            this.qrImageDisplay = new System.Windows.Forms.PictureBox();
+            this.newQrButton = new System.Windows.Forms.Button();
+            this.newQrTextBox = new System.Windows.Forms.TextBox();
+            this.newQrLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.qrImageDisplay)).BeginInit();
             this.SuspendLayout();
             // 
-            // qrImage
+            // qrImageDisplay
             // 
-            this.qrImage.Location = new System.Drawing.Point(333, 150);
-            this.qrImage.Name = "qrImage";
-            this.qrImage.Size = new System.Drawing.Size(177, 185);
-            this.qrImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.qrImage.TabIndex = 0;
-            this.qrImage.TabStop = false;
+            this.qrImageDisplay.Location = new System.Drawing.Point(371, 70);
+            this.qrImageDisplay.Name = "qrImageDisplay";
+            this.qrImageDisplay.Size = new System.Drawing.Size(180, 180);
+            this.qrImageDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.qrImageDisplay.TabIndex = 0;
+            this.qrImageDisplay.TabStop = false;
+            // 
+            // newQrButton
+            // 
+            this.newQrButton.Location = new System.Drawing.Point(636, 98);
+            this.newQrButton.Name = "newQrButton";
+            this.newQrButton.Size = new System.Drawing.Size(155, 23);
+            this.newQrButton.TabIndex = 1;
+            this.newQrButton.Text = "Yeni QR Kod Oluştur";
+            this.newQrButton.UseVisualStyleBackColor = true;
+            this.newQrButton.Click += new System.EventHandler(this.newQrButton_Click);
+            // 
+            // newQrTextBox
+            // 
+            this.newQrTextBox.Location = new System.Drawing.Point(636, 70);
+            this.newQrTextBox.Name = "newQrTextBox";
+            this.newQrTextBox.Size = new System.Drawing.Size(155, 22);
+            this.newQrTextBox.TabIndex = 2;
+            // 
+            // newQrLabel
+            // 
+            this.newQrLabel.AutoSize = true;
+            this.newQrLabel.Location = new System.Drawing.Point(557, 73);
+            this.newQrLabel.Name = "newQrLabel";
+            this.newQrLabel.Size = new System.Drawing.Size(73, 16);
+            this.newQrLabel.TabIndex = 3;
+            this.newQrLabel.Text = "Yazı Verisi:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(845, 493);
-            this.Controls.Add(this.qrImage);
+            this.Controls.Add(this.newQrLabel);
+            this.Controls.Add(this.newQrTextBox);
+            this.Controls.Add(this.newQrButton);
+            this.Controls.Add(this.qrImageDisplay);
             this.Name = "Form1";
             this.Text = "QR Otomasyon";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.qrImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qrImageDisplay)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,7 +90,10 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox qrImage;
+        private System.Windows.Forms.PictureBox qrImageDisplay;
+        private System.Windows.Forms.Button newQrButton;
+        private System.Windows.Forms.TextBox newQrTextBox;
+        private System.Windows.Forms.Label newQrLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,14 +21,17 @@ namespace qr_automation_app
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            byte[] qrCodeImage = BitmapByteQRCodeHelper.GetQRCode("Hello World", QRCodeGenerator.ECCLevel.Q, 5);
-            Bitmap bmp;
-            using (var ms = new MemoryStream(qrCodeImage))
-            {
-                bmp = new Bitmap(ms);
-            }
+            
+        }
 
-            qrImage.Image = bmp;
+        private void newQrButton_Click(object sender, EventArgs e)
+        {
+            byte[] qrCodeImage = BitmapByteQRCodeHelper.GetQRCode(newQrTextBox.Text, QRCodeGenerator.ECCLevel.Q, 5);
+            var ms = new MemoryStream(qrCodeImage);
+            Bitmap bmp = new Bitmap(ms);
+            bmp = new Bitmap(ms);
+
+            qrImageDisplay.Image = bmp;
         }
     }
 }
